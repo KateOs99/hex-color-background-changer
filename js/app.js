44027aae05a7cbb5537ff4f45ca937e81157cd4a
KateOs99 <osadchaya.ket@gmail.com>
2023-10-19 18:37:45 +0200

    funcion de generacion de color aleatorio

--- js/app.js
+++ js/app.js
@@ -2,3 +2,15 @@
 
 document.querySelector("body").style.backgroundColor = "#ff6347";
 
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * simbolos.length)]; // Math.floor = parseInt
+	}
+	 document.body.style.background = color;
+	 document.getElementById("hexadecimal").innerHTML = color;
+     /*document.querySelector("#hexadecimal").textContent = color;*/
+}
